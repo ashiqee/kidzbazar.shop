bb74fffa4d0c5e6ba24767e48116988985431338
ashiqee <ashiq.buet73@gmail.com>
2024-10-28 23:37:25 +0600

    card ui design

--- src/components/ui/cards/ProductCard.tsx
+++ src/components/ui/cards/ProductCard.tsx
@@ -1,7 +1,6 @@
 'use client'
 import React from 'react';
-
-import {Card, CardBody, CardFooter, Image} from "@nextui-org/react";
+import {Button, Card, CardBody, CardFooter, Image} from "@nextui-org/react";
 
 
 
@@ -12,42 +11,42 @@ const ProductCard = () => {
         {
           title: "Orange",
           img: "https://htmldemo.net/kidol/kidol/assets/img/shop/details/1.jpg",
-          price: "$5.50",
+          price: "105.50",
         },
         {
           title: "Tangerine",
           img: "https://htmldemo.net/kidol/kidol/assets/img/shop/details/1.jpg",
-          price: "$3.00",
+          price: "103.00",
         },
         {
           title: "Raspberry",
           img: "https://htmldemo.net/kidol/kidol/assets/img/shop/details/1.jpg",
-          price: "$10.00",
+          price: "1010.00",
         },
         {
           title: "Lemon",
           img: "https://htmldemo.net/kidol/kidol/assets/img/shop/details/1.jpg",
-          price: "$5.30",
+          price: "105.30",
         },
         {
           title: "Avocado",
           img: "https://htmldemo.net/kidol/kidol/assets/img/shop/details/1.jpg",
-          price: "$15.70",
+          price: "1015.70",
         },
         {
           title: "Lemon 2",
           img: "https://htmldemo.net/kidol/kidol/assets/img/shop/details/1.jpg",
-          price: "$8.00",
+          price: "108.00",
         },
         {
           title: "Banana",
           img: "https://htmldemo.net/kidol/kidol/assets/img/shop/details/1.jpg",
-          price: "$7.50",
+          price: "107.50",
         },
         {
           title: "Watermelon",
           img: "https://htmldemo.net/kidol/kidol/assets/img/shop/details/1.jpg",
-          price: "$12.20",
+          price: "1012.20",
         },
       ];
 
@@ -56,20 +55,23 @@ const ProductCard = () => {
     return (
         <div className="gap-2 md:gap-4 grid grid-cols-2 sm:grid-cols-4">
       {list.map((item, index) => (
-        <Card shadow="sm" key={index} isPressable onPress={() => console.log("item pressed")}>
+        <Card key={index} isPressable shadow="sm" onPress={() => console.log("item pressed")}>
           <CardBody className="overflow-visible p-0">
             <Image
-              shadow="sm"
-              radius="lg"
-              width="100%"
               alt={item.title}
-              className="w-full object-cover h-[240px]"
+              className="w-full rounded-none object-cover h-[240px]"
+              radius="lg"
+              shadow="sm"
               src={item.img}
+              width="100%"
             />
           </CardBody>
           <CardFooter className="text-small justify-between">
-            <b>{item.title}</b>
-            <p className="text-default-500">{item.price}</p>
+           <div className='flex flex-col gap-1'>
+           <b>{item.title}</b>
+           <p className="text-default-500">৳ {item.price}</p>
+           </div>
+            <p className='p-2 px-4 bg-slate-400/75 rounded-md'>Add Cart</p>
           </CardFooter>
         </Card>
       ))}
--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + NextUI",
+  name: "Kidz Bazar",
   description: "Make beautiful websites regardless of your design experience.",
   navItems: [
     {
@@ -9,8 +9,8 @@ export const siteConfig = {
       href: "/",
     },
     {
-      label: "Docs",
-      href: "/docs",
+      label: "Shop",
+      href: "/shop",
     },
     {
       label: "Pricing",
